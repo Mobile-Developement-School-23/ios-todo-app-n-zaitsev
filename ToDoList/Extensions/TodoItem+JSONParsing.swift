09df832b0ab86db8a0241914f202cac800aa3bea
n-zaitsev <zajatsnik@gmail.com>
2023-06-13 12:39:43 +0200

    Fix deadline parse and add tests for json parsing

--- ToDoList/Extensions/TodoItem+JSONParsing.swift
+++ ToDoList/Extensions/TodoItem+JSONParsing.swift
@@ -10,30 +10,30 @@ extension TodoItem {
         data[Keys.id.rawValue] = id
         data[Keys.text.rawValue] = text
         data[Keys.creationDate.rawValue] = creationDate.timeIntervalSince1970
-        data[Keys.importance.rawValue] = deadline?.timeIntervalSince1970
+        data[Keys.deadline.rawValue] = deadline?.timeIntervalSince1970
         data[Keys.changeDate.rawValue] = changeDate?.timeIntervalSince1970
         if importance != .ordinary {
             data[Keys.importance.rawValue] = importance.rawValue
         }
         data[Keys.done.rawValue] = done
-        let jsonData = try? JSONSerialization.data(withJSONObject: data)
-        return jsonData as Any
+        return data
     }
 
     static func parse(json: Any) -> TodoItem? {
-        switch json {
-        case let json as Data:
-            return parse(from: json)
-        case let json as String:
-            guard let data = json.data(using: .utf8) else {
-                return nil
-            }
-            return parse(from: data)
-        case let json as [String: Any]:
-            return parse(from: json)
-        default:
-            return nil
+        if let json = json as? [String : Any] {
+            let deadline = json[Keys.deadline.rawValue] as? TimeInterval
+            let changeDate = json[Keys.changeDate.rawValue] as? TimeInterval
+            return TodoItem(
+                            id: json[Keys.id.rawValue] as? String,
+                            text: json[Keys.text.rawValue] as? String ?? "",
+                            creationDate: Date(timeIntervalSince1970: json[Keys.creationDate.rawValue] as? TimeInterval ?? TimeInterval()),
+                            deadline: deadline != nil ? Date(timeIntervalSince1970: deadline!) : nil,
+                            changeDate: changeDate != nil ? Date(timeIntervalSince1970: changeDate!) : nil,
+                            importance: json[Keys.importance.rawValue] as? Importance ?? .ordinary,
+                            done: json[Keys.done.rawValue] as? Bool ?? false
+                            )
         }
+        return nil
     }
 
     enum Keys: String {
@@ -44,31 +44,5 @@ extension TodoItem {
         case changeDate = "change_date"
         case importance
         case done
-    }
-
-    private static func parse(from dict: [String: Any]) ->  TodoItem {
-        let deadline = dict[Keys.deadline.rawValue] as? TimeInterval
-        let changeDate = dict[Keys.changeDate.rawValue] as? TimeInterval
-        return TodoItem(
-                        id: dict[Keys.id.rawValue] as? String,
-                        text: dict[Keys.text.rawValue] as? String ?? "",
-                        creationDate: Date(timeIntervalSince1970: dict[Keys.creationDate.rawValue] as? TimeInterval ?? TimeInterval()),
-                        deadline: deadline != nil ? Date(timeIntervalSince1970: deadline!) : nil,
-                        changeDate: changeDate != nil ? Date(timeIntervalSince1970: changeDate!) : nil,
-                        importance: dict[Keys.importance.rawValue] as? Importance ?? .ordinary,
-                        done: dict[Keys.done.rawValue] as? Bool ?? false
-                        )
-    }
-
-    private static func parse(from data: Data) -> TodoItem? {
-        do {
-            if let dict = try JSONSerialization.jsonObject(with: data) as? [String: Any] {
-                return parse(from: dict)
-            }
-            return nil
-        } catch let error as NSError {
-            print(error.localizedDescription)
-            return nil
-        }
     }
 }
--- ToDoListTests/TodoItemTests.swift
+++ ToDoListTests/TodoItemTests.swift
@@ -93,4 +93,101 @@ final class TodoItemTests: XCTestCase {
             XCTAssertEqual(item.text, text)
         }
     }
+
+    func test_TodoItem_json_shouldMakeJSON() {
+        let item = TodoItem(
+                            id: UUID().uuidString,
+                            text: UUID().uuidString,
+                            creationDate: Date(),
+                            deadline: nil,
+                            changeDate: nil,
+                            importance: .ordinary,
+                            done: Bool.random()
+                    )
+        let json = item.json
+        XCTAssertNotNil(json)
+        XCTAssertNotNil(json as? [String : Any])
+        XCTAssertNoThrow(try JSONSerialization.data(withJSONObject: json))
+    }
+
+    func test_TodoItem_json_shouldNotContainOrdinaryImportance_stress() {
+        for _ in 0..<10 {
+            let array: [TodoItem.Importance] = [.unimportant, .ordinary, .important]
+            let importance = array.randomElement()!
+            let item = TodoItem(
+                                id: UUID().uuidString,
+                                text: UUID().uuidString,
+                                creationDate: Date(),
+                                deadline: nil,
+                                changeDate: nil,
+                                importance: importance,
+                                done: Bool.random()
+                        )
+            let json = item.json as? [String : Any]
+            let contains = json!.contains(where: { $0.key == TodoItem.Keys.importance.rawValue})
+            XCTAssertEqual(contains, importance != .ordinary)
+        }
+    }
+
+    func test_TodoItem_json_shouldNotContainNilDates_stress() {
+        for _ in 0..<10 {
+            var deadline: Date? = nil
+            if Bool.random() {
+                deadline = Date() + TimeInterval.random(in: 0..<100)
+            }
+            var changeDate: Date? = nil
+            if Bool.random() {
+                changeDate = Date() + TimeInterval.random(in: 0..<100)
+            }
+            let item = TodoItem(
+                                id: UUID().uuidString,
+                                text: UUID().uuidString,
+                                creationDate: Date(),
+                                deadline: deadline,
+                                changeDate: changeDate,
+                                importance: .ordinary,
+                                done: Bool.random()
+                        )
+            let json = item.json as? [String: Any]
+            let containsDeadline = json!.contains(where: { $0.key == TodoItem.Keys.deadline.rawValue })
+            let containsChangeDate = json!.contains(where: { $0.key == TodoItem.Keys.changeDate.rawValue })
+            XCTAssertEqual(containsDeadline, deadline != nil)
+            XCTAssertEqual(containsChangeDate, changeDate != nil)
+        }
+    }
+
+    func test_TodoItem_parse_shouldBeNilWithNotDict() {
+        let itemFromArray = TodoItem.parse(json: [Any]())
+        let itemFromString = TodoItem.parse(json: UUID().uuidString)
+        let itemFromData = TodoItem.parse(json: Data())
+        let itemFromSet = TodoItem.parse(json: Set<AnyHashable>())
+        XCTAssertNil(itemFromArray)
+        XCTAssertNil(itemFromString)
+        XCTAssertNil(itemFromData)
+        XCTAssertNil(itemFromSet)
+    }
+
+    func test_TodoItem_parse_shouldNotBeNilWithFullJSON() {
+        let dataFromFullJSON = try! Data(contentsOf: Bundle.main.url(forResource: "todoitem_full", withExtension: "json")!)
+        let fullJSON = try! JSONSerialization.jsonObject(with: dataFromFullJSON)
+        let itemFromFullJSON = TodoItem.parse(json: fullJSON)
+        
+        XCTAssertNotNil(itemFromFullJSON)
+        XCTAssertTrue(!itemFromFullJSON!.id.isEmpty)
+        XCTAssertNotNil(itemFromFullJSON?.deadline)
+        XCTAssertNotNil(itemFromFullJSON?.changeDate)
+    }
+
+    func test_TodoItem_parse_shouldNotBeNilWithEmptyJSON() {
+        let dataFromEmptyJSON = try! Data(contentsOf: Bundle.main.url(forResource: "todoitem_empty", withExtension: "json")!)
+        let emptyJSON = try! JSONSerialization.jsonObject(with: dataFromEmptyJSON)
+        let itemFromEmptyJSON = TodoItem.parse(json: emptyJSON)
+        
+        XCTAssertNotNil(itemFromEmptyJSON)
+        XCTAssertNotNil(UUID(uuidString: itemFromEmptyJSON!.id))
+        XCTAssertNil(itemFromEmptyJSON!.deadline)
+        XCTAssertNil(itemFromEmptyJSON!.changeDate)
+        XCTAssertFalse(itemFromEmptyJSON!.done)
+    }
+
 }

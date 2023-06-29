2114fa280e02280d88c10defa9455b0c7c4905de
n-zaitsev <zajatsnik@gmail.com>
2023-06-30 00:02:49 +0200

    Add small lamberjack usage

--- ToDoList/Modules/TaskList/Coordinator/TaskListCoordinator.swift
+++ ToDoList/Modules/TaskList/Coordinator/TaskListCoordinator.swift
@@ -3,6 +3,7 @@
 //
 
 import UIKit
+import CocoaLumberjackSwift
 
 final class TaskListCoordinator: Coordinator {
     var childCoordinators: [Coordinator] = []
@@ -14,9 +15,17 @@ final class TaskListCoordinator: Coordinator {
         fileCache = FileCache()
         self.navigationController = navigationController
     }
-
+    public let fileLogger: DDFileLogger = DDFileLogger()
+    private let filename = "test"
+    private let format =  FileCache.Format.json
     func start() {
-        try? fileCache.load(from: "test", format: .json)
+        setupLogger()
+        do {
+            try fileCache.load(from: filename, format: format)
+            DDLogInfo("Load from file \(filename + format.rawValue) succeded")
+        } catch let error {
+            DDLogError(error.localizedDescription)
+        }
         let items = Array(fileCache.todoItems.values)
         let taskListVC = TaskListViewController(items: items)
         taskListVC.onDetailsViewController = { [weak self, weak taskListVC] item, state, animated in
@@ -28,18 +37,38 @@ final class TaskListCoordinator: Coordinator {
         taskListVC.onDeleteItem = { [weak self, weak taskListVC] item in
             self?.delete(item: item, viewController: taskListVC)
         }
-        taskListVC.saveNewItem = { [weak fileCache] item in
+        taskListVC.saveNewItem = { [weak self, weak fileCache] item in
+            guard let self else {
+                return
+            }
             _ = fileCache?.add(item: item)
-            try? fileCache?.save(to: "test", format: .json)
+            do {
+                try fileCache?.save(to: self.filename, format: self.format)
+                DDLogInfo("Save to file \(self.filename + self.format.rawValue) succeded")
+            } catch let error {
+                DDLogError(error.localizedDescription)
+            }
         }
         navigationController.pushViewController(taskListVC, animated: true)
+    }
+
+    private func setupLogger() {
+        DDLog.add(DDOSLogger.sharedInstance)
+        fileLogger.rollingFrequency = TimeInterval(60*60*24)
+        fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+        DDLog.add(fileLogger, with: .info)
     }
 }
 
 extension TaskListCoordinator {
     func delete(item: TodoItem, viewController: TaskListViewController?) {
         fileCache.remove(forKey: item.id)
-        try? fileCache.save(to: "test", format: .json)
+        do {
+            try fileCache.save(to: filename, format: format)
+            DDLogInfo("Save to file \(filename + format.rawValue) succeded")
+        } catch let error {
+            DDLogError(error.localizedDescription)
+        }
         viewController?.update(with: .init(item: item), action: .remove)
     }
 
@@ -60,10 +89,18 @@ extension TaskListCoordinator {
         taskDetailsVC.onCancelButton = { [weak navigationController] in
             navigationController?.dismiss(animated: true)
         }
-        taskDetailsVC.onSaveButton = { [weak navigationController, weak viewControler, weak self] item in
-            let oldItem = self?.fileCache.add(item: item)
+        taskDetailsVC.onSaveButton = { [weak self, weak navigationController, weak viewControler] item in
+            guard let self else {
+                return
+            }
+            let oldItem = self.fileCache.add(item: item)
             let action: TaskListTableViewActions = oldItem != nil ? .update : .add
-            try? self?.fileCache.save(to: "test", format: .json)
+            do {
+                try self.fileCache.save(to: self.filename, format: self.format)
+                DDLogInfo("Save to file \(self.filename + self.format.rawValue) succeded")
+            } catch let error {
+                DDLogError(error.localizedDescription)
+            }
             viewControler?.update(with: .init(item: item), action: action)
             navigationController?.dismiss(animated: true)
         }

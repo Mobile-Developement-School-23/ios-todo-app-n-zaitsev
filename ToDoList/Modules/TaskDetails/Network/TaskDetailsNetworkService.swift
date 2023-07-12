9e1dd4e594a2919e9a38290336a8ea05cc573aac
n-zaitsev <zajatsnik@gmail.com>
2023-07-12 21:34:40 +0200

    Revert "Merge pull request #6 from Mobile-Developement-School-23/hw_5"
    This reverts commit a7d0b9b56a93ae98f4c7518cee168bd07b1d9b45, reversing
    changes made to c1040149681a647ab433c1efa9b9779fd2b37298.

--- ToDoList/Modules/TaskDetails/Network/TaskDetailsNetworkService.swift
+++ ToDoList/Modules/TaskDetails/Network/TaskDetailsNetworkService.swift
@@ -14,39 +14,31 @@ final class TaskDetailsNetworkService: TaskDetailsNetworkServiceProtocol {
     }
 
     func addItem(_ item: TodoItem, revision: Int32, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .post)
-            request.body = TaskDetailsRequestBody(element: item)
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .post)
+        request.body = TaskDetailsRequestBody(element: item)
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 
     func getItem(with id: String, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .get)
-            request.url += "/\(id)"
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .get)
+        request.url += "/\(id)"
+        networkService.produceRequest(request, completion: completion)
     }
 
     func changeItem(_ item: TodoItem, revision: Int32, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .put)
-            request.url += "/\(item.id)"
-            request.body = TaskDetailsRequestBody(element: item)
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .put)
+        request.url += "/\(item.id)"
+        request.body = TaskDetailsRequestBody(element: item)
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 
     func deleteItem(with id: String, revision: Int32, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .delete)
-            request.url += "/\(id)"
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .delete)
+        request.url += "/\(id)"
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 }
 // swiftlint:enable line_length
--- ToDoList/Modules/TaskList/Network/TaskListNetworkService.swift
+++ ToDoList/Modules/TaskList/Network/TaskListNetworkService.swift
@@ -15,47 +15,36 @@ final class TaskListNetworkService: TaskListNetworkServiceProtocol {
 
     func getTaskList(completion: @escaping (Result<TaskListResponse, Error>) -> Void) {
         let request = TaskListRequest(method: .get)
-        Task {
-            await networkService.produceRequest(request, completion: completion)
-        }
+        networkService.produceRequest(request, completion: completion)
     }
 
     func addItem(_ item: TodoItem, revision: Int32, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .post)
-            request.body = TaskDetailsRequestBody(element: item)
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .post)
+        request.body = TaskDetailsRequestBody(element: item)
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 
     func changeItem(_ item: TodoItem, revision: Int32, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .put)
-            request.url += "/\(item.id)"
-            request.body = TaskDetailsRequestBody(element: item)
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .put)
+        request.url += "/\(item.id)"
+        request.body = TaskDetailsRequestBody(element: item)
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 
     func deleteItem(with id: String, revision: Int32, completion: @escaping (Result<TaskDetailsResponse, Error>) -> Void) {
-        Task {
-            var request = TaskDetailsRequest(method: .delete)
-            request.url += "/\(id)"
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskDetailsRequest(method: .delete)
+        request.url += "/\(id)"
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 
     func updateTaskList(_ list: [TodoItem], revision: Int32, completion: @escaping (Result<TaskListResponse, Error>) -> Void) {
-        Task {
-            var request = TaskListRequest(method: .patch)
-            request.body = TaskListRequestBody(list: list)
-            request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
-
-            await networkService.produceRequest(request, completion: completion)
-        }
+        var request = TaskListRequest(method: .patch)
+        request.body = TaskListRequestBody(list: list)
+        request.headers.updateValue("\(revision)", forKey: "X-Last-Known-Revision")
+        networkService.produceRequest(request, completion: completion)
     }
 }
 // swiftlint:enable line_length
